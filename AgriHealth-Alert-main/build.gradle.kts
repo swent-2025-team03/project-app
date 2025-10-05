7b6a87b3dbaad83d6a245feb9103f35592c28965
Kevin Morgado <kevin.morgado@gmail.com>
2025-10-05 16:37:11 +0200

    fix(sonar) fix sonar build issue
    Add sonar to the libs.versions.toml as reference to
    fix unresolved reference issue.

--- AgriHealth-Alert-main/build.gradle.kts
+++ AgriHealth-Alert-main/build.gradle.kts
@@ -2,11 +2,5 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-}
 
-sonar {
-    properties {
-        property("sonar.projectKey", "swent-2025-team03_project-app")
-        property("sonar.organization", "swent-2025-team03")
-    }
 }
