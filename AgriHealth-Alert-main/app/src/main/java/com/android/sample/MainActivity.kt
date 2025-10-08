eca9217a76e8a465ad776d31a8932401b2da5320
Kevin Morgado <kevin.morgado@gmail.com>
2025-10-08 13:12:16 +0200

    feat(nav): add navigation between screens
    Edit MainActivity.kt to handle basic navigation feature between
    screens
    
    Set SignInScreen as first and goes to overview.
    Passes a NavigationActions to OverviewScreen And Map to handle
    the bottom bar.

--- AgriHealth-Alert-main/app/src/main/java/com/android/sample/MainActivity.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/sample/MainActivity.kt
@@ -12,7 +12,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
 import com.android.sample.resources.C
+import com.android.sample.ui.authentification.SignInScreen
+import com.android.sample.ui.farmer.AddReportScreen
+import com.android.sample.ui.farmer.MapScreen
+import com.android.sample.ui.farmer.OverviewScreen
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.Screen
 import com.android.sample.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +34,57 @@ class MainActivity : ComponentActivity() {
         Surface(
             modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
             color = MaterialTheme.colorScheme.background) {
-              Greeting("Android")
+              AgriHealthApp()
             }
       }
+    }
+  }
+}
+
+@Composable
+fun AgriHealthApp() {
+  val navController = rememberNavController()
+  val navigationActions = NavigationActions(navController)
+  val startDestination =
+      // TODO replace by real authentication check
+      if (true
+      // needs to be replaced with the condition of a user being already signed in
+      ) Screen.Auth.name
+      else Screen.Overview.route
+
+  NavHost(navController = navController, startDestination = startDestination) {
+    navigation(
+        startDestination = Screen.Auth.route,
+        route = Screen.Auth.name,
+    ) {
+      composable(Screen.Auth.route) {
+        SignInScreen(onSignedIn = { navigationActions.navigateTo(Screen.Overview) })
+      }
+    }
+
+    navigation(
+        startDestination = Screen.Overview.route,
+        route = Screen.Overview.name,
+    ) {
+      composable(Screen.Overview.route) {
+        OverviewScreen(
+            onAddReport = { navigationActions.navigateTo(Screen.AddReport) },
+            onSignedOut = { navigationActions.navigateTo(Screen.Auth) },
+            navigationActions = navigationActions,
+        )
+      }
+      composable(Screen.AddReport.route) {
+        AddReportScreen(
+            onDone = { navigationActions.navigateTo(Screen.Overview) },
+            onGoBack = { navigationActions.goBack() })
+      }
+    }
+
+    navigation(
+        startDestination = Screen.Map.route,
+        route = Screen.Map.name,
+    ) {
+      composable(Screen.Map.route) { MapScreen(navigationActions) }
     }
   }
 }
@@ -38,6 +96,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-  SampleAppTheme { Greeting("Android") }
+fun AgriHealthPreview() {
+  SampleAppTheme { AgriHealthApp() }
 }
