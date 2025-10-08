c2c9ae70686bca7005126eb60aa2745415ddaf14
nils-caspar <nils.caspar@epfl.ch>
2025-10-08 18:03:32 +0200

    Rename UserRepositoryProvider for consistent naming

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/authentification/UserRepositoryProvider.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/authentification/UserRepositoryProvider.kt
@@ -4,7 +4,7 @@ package com.android.agrihealth.data.model.authentification
  * Provides a single instance of the repository in the app. `repository` is mutable for testing
  * purposes.
  */
-object UsersRepositoryProvider {
+object UserRepositoryProvider {
   private val _repository: UserRepository by lazy { UserRepositoryFirestore() }
 
   var repository: UserRepository = _repository
