15a3470f3f843d5d541bc7f953363a7075e34651
Kevin Morgado <kevin.morgado@gmail.com>
2025-10-11 15:30:44 +0200

    feat(Repository): add ReportRepositoryLocal
    Add a Local Repository that doesn't access FireStore
    This can be used to test UI without setting values in Firestore.

---
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/repository/ReportRepositoryLocal.kt
@@ -0,0 +1,48 @@
+package com.android.agrihealth.data.repository
+
+import com.android.agrihealth.data.model.Report
+
+class ReportRepositoryLocal : ReportRepository {
+  private val reports: MutableList<Report> = mutableListOf()
+
+  private var nextId = 0
+
+  override fun getNewReportId(): String {
+    return (nextId++).toString()
+  }
+
+  override suspend fun getAllReports(userId: String): List<Report> {
+    return reports.filter { it.farmerId == userId || it.vetId == userId }
+  }
+
+  override suspend fun getReportsByFarmer(farmerId: String): List<Report> {
+    return reports.filter { it.farmerId == farmerId }
+  }
+
+  override suspend fun getReportById(reportId: String): Report? {
+    return reports.find { it.id == reportId }
+        ?: throw Exception("ReportRepositoryLocal: Report not found")
+  }
+
+  override suspend fun addReport(report: Report) {
+    reports.add(report)
+  }
+
+  override suspend fun editReport(reportId: String, newReport: Report) {
+    val index = reports.indexOfFirst { it.id == reportId }
+    if (index != -1) {
+      reports[index] = newReport
+    } else {
+      throw Exception("ReportRepositoryLocal: Report not found")
+    }
+  }
+
+  override suspend fun deleteReport(reportId: String) {
+    val index = reports.indexOfFirst { it.id == reportId }
+    if (index != -1) {
+      reports.removeAt(index)
+    } else {
+      throw Exception("ReportRepositoryLocal: Report not found")
+    }
+  }
+}
