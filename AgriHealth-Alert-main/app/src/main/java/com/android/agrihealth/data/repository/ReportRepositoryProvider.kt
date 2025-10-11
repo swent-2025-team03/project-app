67cd070c0cff3eb9e63941f73e55c507ab5f7a8b
Kevin Morgado <kevin.morgado@gmail.com>
2025-10-11 20:42:21 +0200

    fix(repo): change the repo provided
    Change the ReportRepositoryProvider.kt to give the
    local repo because the firestore version is
    untested.

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/repository/ReportRepositoryProvider.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/repository/ReportRepositoryProvider.kt
@@ -9,7 +9,7 @@ object ReportRepositoryProvider {
    * Provides a single instance of the repository in the app. `repository` is mutable for testing
    * purposes.
    */
-  private val _repository: ReportRepository by lazy { ReportRepositoryFirestore(Firebase.firestore) }
+  private val _repository: ReportRepository by lazy { ReportRepositoryLocal() }
 
   var repository: ReportRepository = _repository
 }
