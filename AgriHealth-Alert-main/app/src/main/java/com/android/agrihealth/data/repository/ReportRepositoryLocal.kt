d1917a03a3ce3480e87cf339bb29736246b59ff8
Kevin Morgado <kevin.morgado@gmail.com>
2025-10-11 22:03:53 +0200

    doc(repo): add doc for local repo

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/repository/ReportRepositoryLocal.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/repository/ReportRepositoryLocal.kt
@@ -2,6 +2,9 @@ package com.android.agrihealth.data.repository
 
 import com.android.agrihealth.data.model.Report
 
+/**
+ * Local in-memory implementation of ReportRepository for testing and development purposes.
+ */
 class ReportRepositoryLocal : ReportRepository {
   private val reports: MutableList<Report> = mutableListOf()
 
