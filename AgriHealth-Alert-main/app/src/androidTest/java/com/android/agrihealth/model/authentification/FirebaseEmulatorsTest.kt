0d653647480f23c8a8f7cc7edbad2fa569cacdf8
nils-caspar <nils.caspar@epfl.ch>
2025-10-14 17:21:26 +0200

    feat(firebase): Implement Firebase permissions test (WIP)

--- AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/model/authentification/FirebaseEmulatorsTest.kt
+++ AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/model/authentification/FirebaseEmulatorsTest.kt
@@ -11,17 +11,48 @@ import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Before
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 open class FirebaseEmulatorsTest {
     val userRepository = UserRepositoryProvider.repository
     val authRepository = AuthRepositoryProvider.repository
+
+    //from bootcamp
+    val httpClient = OkHttpClient()
+    //val host = "localhost"
+    val contextHost = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
+    val host = contextHost.getString(R.string.FIREBASE_EMULATORS_URL)
+    val firestorePort = 8081
+    val authPort = 9099
+    private val firestoreEndpoint by lazy {
+        "http://${host}:${firestorePort}/emulator/v1/projects/agrihealth-alert/databases/(default)/documents"
+    }
+
+    private val authEndpoint by lazy {
+        "http://${host}:${authPort}/emulator/v1/projects/agrihealth-alert/accounts"
+    }
 
 
 
     val user1 = User("abc123", "Rushia", "Uruha", UserRole.FARMER, "email1@thing.com")
     val user2 = User("def456", "mike", "neko", UserRole.FARMER, "email2@aaaaa.balls")
     val user3 = User("ghj789", "Nazuna", "Amemiya", UserRole.VETERINARIAN, "email3@kms.josh")
+
+    val password1 = "Password123"
+    val password2 = "iamaweakpassword"
+    val password3 = "12345678"
+
+    //from Bootcamp
+    private fun clearEmulator(endpoint: String) {
+        val client = httpClient
+        val request = Request.Builder().url(endpoint).delete().build()
+        val response = client.newCall(request).execute()
+
+        assert(response.isSuccessful) { "Failed to clear emulator at $endpoint" }
+    }
 
     private suspend fun clearUsers() {
         val usersCollection = Firebase.firestore.collection(USERS_COLLECTION_PATH).get().await()
@@ -42,6 +73,7 @@ open class FirebaseEmulatorsTest {
         if (!emulatorInitialized) {
             val context = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
             val url = context.getString(R.string.FIREBASE_EMULATORS_URL)
+            //val url = "localhost"
             val firestorePort = context.resources.getInteger(R.integer.FIREBASE_EMULATORS_FIRESTORE_PORT)
             val authPort = context.resources.getInteger(R.integer.FIREBASE_EMULATORS_AUTH_PORT)
 
@@ -51,6 +83,10 @@ open class FirebaseEmulatorsTest {
             emulatorInitialized = true
         }
 
-        runTest { clearUsers() }
+        runTest {
+            //clearUsers()
+            clearEmulator(authEndpoint)
+            clearEmulator(firestoreEndpoint)
+        }
     }
 }
--- AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/model/authentification/PermissionsFirestoreTest.kt
+++ AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/model/authentification/PermissionsFirestoreTest.kt
@@ -3,26 +3,61 @@ package com.android.agrihealth.model.authentification
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
+import com.google.firebase.auth.FirebaseAuth
+import org.junit.Assert.*
+import com.google.firebase.emulators.EmulatedServiceSettings
+import com.google.firebase.firestore.FirebaseFirestoreException
+import org.junit.After
 
 class PermissionsFirestoreTest : FirebaseEmulatorsTest() {
+  val auth = FirebaseAuth.getInstance()
+
+
   @Before
   override fun setUp() {
     super.setUp()
+    authRepository.signOut()
   }
 
   @Test
-  fun canAccessOwnData() {
-    TODO()
+  fun canAccessOwnData() = runTest {
+    authRepository.signUpWithEmailAndPassword(user1.email, password1, user1)
+    //authRepository.signInWithEmailAndPassword(user1.email, password1)
+
+    assertNotNull(auth.currentUser)
+    val uid = auth.currentUser?.uid
+    assertNotNull(uid)
+
+    val userData = userRepository.getUserFromId(uid!!).getOrThrow()
+    assertEquals(uid, userData.uid)
+    assertEquals(user1.name, userData.name)
+    assertEquals(user1.surname, userData.surname)
+    assertEquals(user1.role, userData.role)
+    assertEquals(user1.email, userData.email)
   }
 
   @Test
-  fun failToAccessOtherUsersData() {
-    TODO()
+  fun failToAccessOtherUsersData() = runTest {
+    authRepository.signUpWithEmailAndPassword(user2.email, password2, user2)
+    assertNotNull(auth.currentUser)
+    val uid2 = auth.currentUser!!.uid
+
+    authRepository.signOut()
+    authRepository.signUpWithEmailAndPassword(user1.email, password1, user1)
+    assertNotNull(auth.currentUser)
+
+    val result = userRepository.getUserFromId(uid2)
+
+    assertTrue(result.isFailure)
+    val exception = result.exceptionOrNull()
+    assertNotNull(exception)
+    assertTrue(exception is FirebaseFirestoreException)
+    assertEquals((exception as FirebaseFirestoreException).code, FirebaseFirestoreException.Code.PERMISSION_DENIED)
   }
 
   @Test
   fun failToAccessDataWhileLoggedOut() {
-    TODO()
+    //TODO()
   }
 
   @Test
