692a3bc94cdfa7ed47ee6510a53c9fb6a23b45f0
Amerilla <alain.merillat@epfl.ch>
2025-10-15 03:47:51 +0200

    Move report creation screen file for better compatibility with navigation
    ^ Conflicts:
    ^	AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/farmer/AddReportScreen.kt

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/farmer/AddReportViewModel.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/farmer/AddReportViewModel.kt
@@ -1,32 +1,21 @@
-package com.android.agrihealth.ui.report
+package com.android.agrihealth.ui.farmer
 
 import android.graphics.Bitmap
-import android.net.Uri
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.android.agrihealth.data.model.authentification.AuthRepository
-import com.android.agrihealth.data.model.authentification.AuthRepositoryProvider
-import com.android.agrihealth.data.model.authentification.User
-import com.android.agrihealth.data.model.authentification.UserRepository
-import com.android.agrihealth.data.model.authentification.UserRepositoryProvider
-import com.android.agrihealth.data.model.authentification.UserRole
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 
-data class CreateReportUiState(
+data class AddReportUiState(
     val title: String = "",
     val description: String = "",
     val chosenVet: String = "",   // TODO: Shouldn't be a string! Temporary measure
     val imageBitmap: Bitmap? = null    // TODO: Currently just an image but should hold mutliple images/videos later
 )
 
-class CreateReportViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(CreateReportUiState())
-    val uiState: StateFlow<CreateReportUiState> = _uiState
+class AddReportViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(AddReportUiState())
+    val uiState: StateFlow<AddReportUiState> = _uiState
 
     fun setTitle(newTitle: String) {
         _uiState.value = _uiState.value.copy(title = newTitle)
