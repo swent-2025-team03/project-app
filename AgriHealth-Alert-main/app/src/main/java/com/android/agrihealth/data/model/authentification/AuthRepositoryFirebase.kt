50b8cea1f8721bf51e2d147ff35c3e2a6d8c8122
adam_franco <adam.franco@epfl.ch>
2025-10-27 15:45:52 +0100

    ktfmt

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/authentification/AuthRepositoryFirebase.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/authentification/AuthRepositoryFirebase.kt
@@ -2,10 +2,10 @@ package com.android.agrihealth.data.model.authentification
 
 import androidx.credentials.Credential
 import androidx.credentials.CustomCredential
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
 import com.android.agrihealth.data.model.user.Farmer
 import com.android.agrihealth.data.model.user.User
 import com.android.agrihealth.data.model.user.Vet
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
