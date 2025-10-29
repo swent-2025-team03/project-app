96854d2585c92639f614d4c67511c7e6e15d99fe
nils-caspar <nils.caspar@epfl.ch>
2025-10-29 16:45:20 +0100

    feature(map): Implement report fetch

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/map/MapViewModel.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/map/MapViewModel.kt
@@ -1,64 +1,45 @@
 package com.android.agrihealth.ui.map
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.android.agrihealth.data.model.Location
 import com.android.agrihealth.data.model.Report
 import com.android.agrihealth.data.model.ReportStatus
+import com.android.agrihealth.data.repository.ReportRepository
+import com.android.agrihealth.data.repository.ReportRepositoryProvider
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 data class MapUIState(
     val reports: List<Report> = emptyList(),
 )
 
-class MapViewModel : ViewModel() {
-  // Placeholder reports for testing
-  val report1 =
-      Report(
-          "rep_id1",
-          "Report title 1",
-          "Description 1",
-          null,
-          "farmerId1",
-          null,
-          ReportStatus.PENDING,
-          null,
-          Location(46.5200948, 6.5651742, "Place name 1"))
-  val report2 =
-      Report(
-          "rep_id2",
-          "Report title 2",
-          "Description aaaa 2",
-          null,
-          "farmerId2",
-          "vetId2",
-          ReportStatus.IN_PROGRESS,
-          "Vet answer",
-          Location(46.5183104, 6.5676777))
-  val report3 =
-      Report(
-          "rep_id3",
-          "Report title 3",
-          "Description 3",
-          null,
-          "farmerId3",
-          null,
-          ReportStatus.RESOLVED,
-          null,
-          Location(46.5206231, 6.569927, "Place name 3"))
-  val report4 =
-      Report(
-          "rep_id4",
-          "Report title 4",
-          "Description aaaa 4",
-          null,
-          "farmerId4",
-          "vetId4",
-          ReportStatus.ESCALATED,
-          "Vet answer 4",
-          Location(46.5232, 6.5681191))
-
-  private val _uiState = MutableStateFlow(MapUIState(listOf(report1, report2, report3, report4)))
+class MapViewModel(private val reportRepository: ReportRepository = ReportRepositoryProvider.repository) : ViewModel() {
+  private val _uiState = MutableStateFlow(MapUIState())
   val uiState: StateFlow<MapUIState> = _uiState.asStateFlow()
+
+    init {
+        Firebase.auth.addAuthStateListener {
+            if (it.currentUser != null) {
+                fetchLocalizableReports()
+            }
+        }
+    }
+
+    private fun fetchLocalizableReports() {
+        viewModelScope.launch {
+            try {
+                val userId = Firebase.auth.currentUser!!.uid
+                val reports = reportRepository.getAllReports(userId).filter { it.location != null }
+                _uiState.value = MapUIState(reports = reports)
+            } catch (e: Exception) {
+                Log.w("MapScreen", "Failed to load todos: ${e.message}")
+            }
+        }
+    }
 }
