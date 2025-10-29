ee31b9c8cf50acd7cca6ae402c96d8eb4f7d4ed8
nils-caspar <nils.caspar@epfl.ch>
2025-10-29 19:19:37 +0100

    chore(user): ktfmtFormat

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/user/Farmer.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/user/Farmer.kt
@@ -7,7 +7,8 @@ data class Farmer(
     override val firstname: String,
     override val lastname: String,
     override val email: String,
-    override val address: Location?, // This should be the farm location, set just after creating an account.
+    override val address:
+        Location?, // This should be the farm location, set just after creating an account.
     val linkedVets: List<String> =
         emptyList<String>(), // List of vet IDs associated with the farmer
     val defaultVet: String? // Default vet ID for quick access, can be changed in profile screen
