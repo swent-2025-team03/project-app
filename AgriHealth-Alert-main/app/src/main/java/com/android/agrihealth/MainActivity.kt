3def83a79143e3989473ba6ea3918506c61a56ae
nils-caspar <nils.caspar@epfl.ch>
2025-10-31 14:11:02 +0100

    fix(overview): ktfmtFormat

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/MainActivity.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/MainActivity.kt
@@ -8,11 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.semantics
@@ -146,9 +146,7 @@ fun AgriHealthApp(
             onBack = { navigationActions.goBack() },
             userRole = currentUserRole,
             userId = currentUserId,
-            onCreateReport = {
-              reloadReports = !reloadReports
-            },
+            onCreateReport = { reloadReports = !reloadReports },
             addReportViewModel = createReportViewModel,
         )
       }
