3b878eae0c6ac64b42c861c65f28729c8911bc88
Kevin Morgado <kevin.morgado@gmail.com>
2025-11-02 15:02:00 +0100

    Refactor(repo): switch the report repo provider to firestore.
    this will allow to debug more easily using the emulator.

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/repository/ReportRepositoryProvider.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/repository/ReportRepositoryProvider.kt
@@ -1,12 +1,15 @@
 package com.android.agrihealth.data.repository
 
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
 object ReportRepositoryProvider {
 
   /**
    * Provides a single instance of the repository in the app. `repository` is mutable for testing
    * purposes.
    */
-  private val _repository: ReportRepository by lazy { ReportRepositoryLocal() }
+  private val _repository: ReportRepository by lazy { ReportRepositoryFirestore(Firebase.firestore) }
 
   var repository: ReportRepository = _repository
 }
