5a919d6757e9dda8b22019bc0de1d30e056e27d4
adam_franco <adam.franco@epfl.ch>
2025-11-04 13:33:59 +0100

    Add missing import statement.

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/overview/OverviewViewModel.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/overview/OverviewViewModel.kt
@@ -1,5 +1,6 @@
 package com.android.agrihealth.ui.overview
 
+import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
