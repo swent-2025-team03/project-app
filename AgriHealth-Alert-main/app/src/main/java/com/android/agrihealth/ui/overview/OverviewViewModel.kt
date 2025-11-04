58b1f58223325a1f97005afba7dba3da700cbcc7
nils-caspar <127340226+nils-caspar@users.noreply.github.com>
2025-11-04 14:41:49 +0100

    hotfix(overview): Uncomment clearCredentialState (#161)
    * fix(overview): Uncomment clearCredentialState
    
    This line doesn't run on my phone so I keep commenting it out and accidentally committing it, please lynch me if I forget this again
    
    * Add missing import statement.
    
    ---------
    
    Co-authored-by: adam_franco <adam.franco@epfl.ch>

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/overview/OverviewViewModel.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/overview/OverviewViewModel.kt
@@ -1,5 +1,6 @@
 package com.android.agrihealth.ui.overview
 
+import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -71,7 +72,7 @@ class OverviewViewModel(
     authRepository = AuthRepositoryProvider.repository
     viewModelScope.launch {
       authRepository.signOut()
-      // credentialManager.clearCredentialState(ClearCredentialStateRequest())
+      credentialManager.clearCredentialState(ClearCredentialStateRequest())
     }
   }
 }
