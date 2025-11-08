6d5676173de788ae125b75c070ca7cf0d7f3b2ec
adam_franco <adam.franco@epfl.ch>
2025-11-08 12:29:07 +0100

    Fix remaining fill max size on role selection screen

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/authentification/RoleSelectionScreen.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/authentification/RoleSelectionScreen.kt
@@ -2,7 +2,6 @@ package com.android.agrihealth.ui.authentification
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -10,7 +9,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,10 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,8 +55,6 @@ fun RoleSelectionScreen(
     onButtonPressed: () -> Unit = {},
     userViewModel: UserViewModel = viewModel()
 ) {
-  val textColor = Color.Black
-  val buttonColor = Color(0xFFDDF4E7)
   val userGreeting = remember {
     if (Firebase.auth.currentUser!!.displayName != null) {
       "Welcome, ${Firebase.auth.currentUser!!.displayName}!"
@@ -68,7 +62,6 @@ fun RoleSelectionScreen(
   }
 
   Scaffold(
-      modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
       topBar = {
         // Top bar with back arrow and title/status
         TopAppBar(
@@ -85,21 +78,16 @@ fun RoleSelectionScreen(
             })
       }) { padding ->
         Column(
-            modifier = Modifier.padding(padding).fillMaxSize(),
+            modifier = Modifier.padding(padding).verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)) {
               Text(
                   text = userGreeting,
-                  fontWeight = FontWeight.Medium,
                   style = MaterialTheme.typography.displaySmall,
-                  color = textColor,
                   overflow = TextOverflow.Visible,
                   textAlign = TextAlign.Center,
                   modifier = Modifier.testTag(RoleSelectionScreenTestTags.WELCOME).fillMaxWidth())
-              Text(
-                  text = "Please choose a role.",
-                  style = MaterialTheme.typography.headlineMedium,
-                  color = textColor)
+              Text(text = "Please choose a role.", style = MaterialTheme.typography.headlineMedium)
               Button(
                   onClick = {
                     val firebaseUser = Firebase.auth.currentUser
@@ -119,9 +107,8 @@ fun RoleSelectionScreen(
                     vm.createUser(UserRole.FARMER)
                     onButtonPressed.invoke()
                   },
-                  colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
                   modifier = Modifier.testTag(RoleSelectionScreenTestTags.FARMER)) {
-                    Text("I'm a Farmer!", color = textColor)
+                    Text("I'm a Farmer!")
                   }
               Button(
                   onClick = {
@@ -140,9 +127,8 @@ fun RoleSelectionScreen(
                     vm.createUser(UserRole.VET)
                     onButtonPressed.invoke()
                   },
-                  colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
                   modifier = Modifier.testTag(RoleSelectionScreenTestTags.VET)) {
-                    Text("I'm a Veterinarian!", color = textColor)
+                    Text("I'm a Veterinarian!")
                   }
             }
       }
