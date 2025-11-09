dab1b8f55aecdadac69222749d61e51e85125469
nils-caspar <127340226+nils-caspar@users.noreply.github.com>
2025-11-09 17:19:24 +0100

    Feature/location services (#200)
    * feat(location): Implement location features
    
    Adds a location repository (and provider) and view-model to fetch the last known location and/or the current location, a composable to request location permissions, and a temporary test screen
    
    * feat(location): Adds location repository to the main app
    
    * feat(location): Add permission checks
    
    * feat(location): Add test suite
    
    * ktfmtFormat
    
    * fix(location): Change file/function names for clarity
    
    * fix(location): Add instruction usage comments
    
    * fix(location): Comment out preview function for line coverage
    
    * fix(location): Add implementation instruction for permissions
    
    * ktfmtFormat
    
    * fix(location): Comment out code in a smarter way
    
    * ktfmtFormat
    
    * fix(location): Change IllegalStateException line for sonar
    
    * fix(location): Catch generic exception when fetching location
    
    * fix(location): Add check if fetched location is the same as previously
    
    * fix(location): Update permission requester sample usage to prevent crash
    
    * ktfmtFormat
    
    * when the boolean is true
    
    * fix(location): Fix lazy copypaste in ViewModel
    
    * fix(location): Add @Before to tests

---
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/device/location/LocationViewModel.kt
@@ -0,0 +1,78 @@
+package com.android.agrihealth.data.model.device.location
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.agrihealth.data.model.location.Location
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class LocationViewModel() : ViewModel() {
+  private val locationRepository = LocationRepositoryProvider.repository
+
+  private val _locationState = MutableStateFlow<Location?>(null)
+  val locationState = _locationState.asStateFlow()
+
+  private val exceptionLogTag = "LocationServices"
+
+  private fun securityExceptionLogMsg(e: SecurityException) =
+      "Missing permissions for location: ${e.message}"
+
+  private fun genericExceptionLogMsg(e: Exception) = "Something went wrong: ${e.message}"
+
+  private fun throwIfNotAllowed() {
+    check(hasLocationPermissions()) { "Location permissions not granted" }
+  }
+
+  private fun locationsEqual(l1: Location?, l2: Location?): Boolean {
+    if (l1 == null || l2 == null) return false
+    return (l1.latitude == l2.latitude) && (l1.longitude == l2.longitude)
+  }
+
+  /**
+   * Gets the last known location, or, if unavailable, the current device location. Make sure the
+   * user has given permissions first, for example using the "LocationPermissionsRequester"
+   * composable
+   */
+  fun getLastKnownLocation() {
+    throwIfNotAllowed()
+    viewModelScope.launch {
+      try {
+        val location = locationRepository.getLastKnownLocation()
+        if (!locationsEqual(location, _locationState.value))
+            _locationState.value = locationRepository.getLastKnownLocation()
+      } catch (e: SecurityException) {
+        Log.e(exceptionLogTag, securityExceptionLogMsg(e))
+      } catch (e: Exception) {
+        Log.e(exceptionLogTag, genericExceptionLogMsg(e))
+      }
+    }
+  }
+
+  /**
+   * Gets the current device location. More expensive on the battery than last known location. Make
+   * sure the user has given permissions first, for example using the "LocationPermissionsRequester"
+   * composable
+   */
+  fun getCurrentLocation() {
+    throwIfNotAllowed()
+    viewModelScope.launch {
+      try {
+        val location = locationRepository.getCurrentLocation()
+        if (!locationsEqual(location, _locationState.value))
+            _locationState.value = locationRepository.getCurrentLocation()
+      } catch (e: SecurityException) {
+        Log.e(exceptionLogTag, securityExceptionLogMsg(e))
+      } catch (e: Exception) {
+        Log.e(exceptionLogTag, genericExceptionLogMsg(e))
+      }
+    }
+  }
+
+  /** Checks if the user allowed all device location permissions on the app */
+  fun hasLocationPermissions(): Boolean {
+    return locationRepository.hasFineLocationPermission() &&
+        locationRepository.hasCoarseLocationPermission()
+  }
+}
