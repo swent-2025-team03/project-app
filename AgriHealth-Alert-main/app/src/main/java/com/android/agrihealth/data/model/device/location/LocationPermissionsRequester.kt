f524147434f0aa9356df317b8e92e281b41ef5b7
nils-caspar <nils.caspar@epfl.ch>
2025-11-09 13:52:57 +0100

    fix(location): Update permission requester sample usage to prevent crash

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/device/location/LocationPermissionsRequester.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/device/location/LocationPermissionsRequester.kt
@@ -2,19 +2,30 @@ package com.android.agrihealth.data.model.device.location
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun LocationPermissionsRequester(locationViewModel: LocationViewModel = viewModel()) {
+fun locationPermissionsRequester(locationViewModel: LocationViewModel = viewModel()): Boolean {
   val context = LocalContext.current
   val permissions =
       arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -36,6 +47,7 @@ fun LocationPermissionsRequester(locationViewModel: LocationViewModel = viewMode
     if (!granted.value) locationPermissionRequest.launch(permissions)
     else locationViewModel.getLastKnownLocation()
   }
+    return granted.value
 }
 
 // TODO: Remove this block once location services are implemented somewhere else in the app
@@ -49,15 +61,15 @@ fun LocationTestScreen() {
 
     val location by viewModel.locationState.collectAsState()
 
-    LocationPermissionsRequester()
-    viewModel.getCurrentLocation()
+    if (locationPermissionsRequester()) viewModel.getCurrentLocation()
 
     Column(Modifier.padding(16.dp).background(color = White)) {
       Text("Device Location")
       if (location != null) {
         Text("Lat: ${location!!.latitude}, Lng: ${location!!.longitude}")
       } else {
-        Text("Fetching location...")
+          if (viewModel.hasLocationPermissions()) Text("Fetching location...")
+          else Text("Permission denied")
       }
     }
   }
