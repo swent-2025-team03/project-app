2cf7ecd94ffc21999308a299f965a566e28a71ad
Amerilla <alain.merillat@epfl.ch>
2025-11-11 04:32:58 +0100

    Improve documentation

--- AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/utils/TestAssetUtils.kt
+++ AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/utils/TestAssetUtils.kt
@@ -4,16 +4,26 @@ import android.net.Uri
 import androidx.test.platform.app.InstrumentationRegistry
 import java.io.File
 
+/**
+ *    Utility class used during testing when dealing with test assets such as pictures, videos, etc...
+ */
 object TestAssetUtils {
 
   // Constants for commonly used test files
+  /** Fake photo used when creating a new report */
   const val FAKE_PHOTO_FILE = "report_image_cat.jpg"
 
-  fun getUriFrom(assetName: String): Uri {
+  /**
+   *   Returns the Uri of a given file. File must be located in "androidTest/assets"
+   *
+   *   @property fileName Name of the file
+   *   @return The Uri of the given file
+   */
+  fun getUriFrom(fileName: String): Uri {
     val instrumentation = InstrumentationRegistry.getInstrumentation()
-    val cacheFile = File(instrumentation.targetContext.cacheDir, assetName)
+    val cacheFile = File(instrumentation.targetContext.cacheDir, fileName)
 
-    instrumentation.context.assets.open(assetName).use { input ->
+    instrumentation.context.assets.open(fileName).use { input ->
       cacheFile.outputStream().use { output -> input.copyTo(output) }
     }
 
