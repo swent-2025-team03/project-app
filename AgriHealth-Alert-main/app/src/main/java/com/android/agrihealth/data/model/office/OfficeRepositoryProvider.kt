a741a1cae4a6762f315d4e16dca2c0b16968f2ff
MadaMada08 <adam.cohen@epfl.ch>
2025-12-05 22:28:04 +0100

    fix(E2ETest): fix failing E2ETest to follow new filtering logic

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/office/OfficeRepositoryProvider.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/office/OfficeRepositoryProvider.kt
@@ -1,7 +1,16 @@
 package com.android.agrihealth.data.model.office
 
 object OfficeRepositoryProvider {
+  private var overrideRepo: OfficeRepository? = null
   private val firestoreRepository by lazy { OfficeRepositoryFirestore() }
 
-  fun get(): OfficeRepository = firestoreRepository
+  fun set(repo: OfficeRepository) {
+    overrideRepo = repo
+  }
+
+  fun reset() {
+    overrideRepo = null
+  }
+
+  fun get(): OfficeRepository = overrideRepo ?: firestoreRepository
 }
