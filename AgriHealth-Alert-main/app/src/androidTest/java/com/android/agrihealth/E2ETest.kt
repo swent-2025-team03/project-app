bfc2df011f7fee2f99e64a2267fb9176aebb262c
MadaMada08 <adam.cohen@epfl.ch>
2025-12-09 09:03:07 +0100

    fix(test): adapt E2E test

--- AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/E2ETest.kt
+++ AgriHealth-Alert-main/app/src/androidTest/java/com/android/agrihealth/E2ETest.kt
@@ -467,6 +467,8 @@ class E2ETest : FirebaseEmulatorsTest() {
       composeTestRule.onNodeWithText("Discard changes").isDisplayed()
     }
     composeTestRule.onNodeWithText("Discard changes").performClick()
+
+    waitUntilTestTag(OverviewScreenTestTags.SCREEN)
   }
 
   private fun checkAssignedVetDisplayedOnOverview() {
