8977a827000f9a9203c0ccd8633896a9f3531854
adam_franco <adam.franco@epfl.ch>
2025-12-13 17:01:58 +0100

    address Review.

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/helpers/runWithTimeout.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/data/model/helpers/runWithTimeout.kt
@@ -4,10 +4,12 @@ import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withTimeout
 
+const val TIMEOUT = 2_000L
+
 /**
  * Helper function to let Tasks timeout if they are used in poor connections settings or if they are
  * offline.
  */
 suspend fun <T> runWithTimeout(block: Task<T>): T {
-  return withTimeout(2_000L) { block.await() }
+  return withTimeout(TIMEOUT) { block.await() }
 }
