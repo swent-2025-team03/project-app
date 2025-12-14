4fa0965764f20ebe674a6dfe3e117c37917498c3
adam_franco <adam.franco@epfl.ch>
2025-12-14 11:21:21 +0100

    clear focus when clicking on empty space.

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/MainActivity.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.MaterialTheme
@@ -19,7 +20,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.tooling.preview.Preview
@@ -94,10 +97,14 @@ class MainActivity : ComponentActivity() {
 
     setContent {
       AgriHealthAppTheme {
+        val focusManager = LocalFocusManager.current
+        val clearFocusModifier =
+            Modifier.pointerInput(Unit) { detectTapGestures(onTap = { focusManager.clearFocus() }) }
         // A surface container using the 'background' color from the theme
         Surface(
             modifier =
-                Modifier.fillMaxSize()
+                clearFocusModifier
+                    .fillMaxSize()
                     .semantics { testTag = C.Tag.main_screen_container }
                     .navigationBarsPadding(),
             color = MaterialTheme.colorScheme.background) {
