ed97717b4ce05a44c55a0945a1d582e37509264e
Kevin Morgado <kevin.morgado@epfl.ch>
2025-12-14 12:54:00 +0100

    chore(ManageOffice): remove todo.
    - Implemented simply forgot to remove the comment

--- AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/office/ManageOfficeScreen.kt
+++ AgriHealth-Alert-main/app/src/main/java/com/android/agrihealth/ui/office/ManageOfficeScreen.kt
@@ -182,7 +182,6 @@ fun ManageOfficeScreen(
                         modifier = Modifier.fillMaxWidth().testTag(LEAVE_OFFICE_BUTTON)) {
                           Text("Leave My Office")
                         }
-                    // TODO: improve leave button permissions; currently only owners can trigger
                     // dialog
 
                     if (showLeaveDialog) {
